32bce8ea346c0eb33484c06562f9a2168643470b
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-09-17 20:56:53 -0500

    eslint vuuue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script></script>
-
 <template>
   <div id="app">
     <router-view />
   </div>
 </template>
+
+<script setup></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { createMemoryHistory, createRouter } from "vue-router";
-import Home from './pages/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from './pages/Home.vue';
 
-
-
-
+// Define your routes
 const routes = [
   { path: "/", component: Home },
 ];
 
+// Create the router instance
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(), // Use createWebHistory for web applications
   routes,
 });
 
-
+// Create and mount the Vue application
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
